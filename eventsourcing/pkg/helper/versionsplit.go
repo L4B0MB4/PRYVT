6a4bdddea4e8015c493224c9befb8bc520003264
L4B0MB4 <metalfan65@gmail.com>
2024-09-13 15:53:32 +0200

    eventsourcing: adding tests for version split and merge

--- eventsourcing/pkg/helper/versionsplit.go
+++ eventsourcing/pkg/helper/versionsplit.go
@@ -6,8 +6,8 @@ func SplitVersion(version int64) (int32, int32, error) {
 	if version < 0 || (version<<1) < 0 {
 		return 0, 0, errors.New("NOT SUPPORTING USAGE OF MORE THAN 62 BITS OF THE 64 INTEGER")
 	}
-	low := int32(version & 0xFFFFFFFF)
-	high := int32(version >> 32)
+	low := int32(version & 0x7FFF_FFFF)
+	high := int32(version >> 31)
 	return high, low, nil
 }
 
@@ -17,7 +17,7 @@ func MergeVersion(high int32, low int32) (int64, error) {
 	}
 
 	high64 := int64(high)
-	high64 = high64 << 32
+	high64 = high64 << 31
 	low64 := int64(low)
 	value := high64 + low64
 
--- eventsourcing/test/helper_test.go
+++ eventsourcing/test/helper_test.go
@@ -7,8 +7,7 @@ import (
 	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/helper"
 )
 
-func TestVersionSplitting(t *testing.T) {
-
+func TestVersionSplitting64Bit(t *testing.T) {
 	var b1 int64 = int64(math.Pow(2, 64)) - 1
 
 	_, _, err := helper.SplitVersion(b1)
@@ -16,21 +15,110 @@ func TestVersionSplitting(t *testing.T) {
 		t.Error("Should have thrown an error with 64 bit integer")
 		t.Fail()
 	}
-	b1 = int64(math.Pow(2, 63)) - 1
-	_, _, err = helper.SplitVersion(b1)
+}
+func TestVersionSplitting63Bit(t *testing.T) {
+	var b1 int64 = int64(math.Pow(2, 63)) - 1
+
+	_, _, err := helper.SplitVersion(b1)
 	if err == nil {
 		t.Error("Should have thrown an error with 63 bit integer")
 		t.Fail()
 	}
-	b1 = int64(math.Pow(2, 62)) - 1
+}
+
+func TestVersionSplitting62Bit(t *testing.T) {
+
+	var b1 int64 = int64(math.Pow(2, 62)) - 1
+
+	_, _, err := helper.SplitVersion(b1)
+	if err != nil {
+		t.Error("Should be able to handle 62 bit integer")
+		t.Fail()
+	}
+}
+
+func TestVersionSplitting(t *testing.T) {
+
+	var b1 int64 = int64(math.Pow(2, 62)) - 1
 
 	i1, i2, err := helper.SplitVersion(b1)
 	if err != nil {
 		t.Error("Should be able to handle 62 bit integer")
 		t.Fail()
 	}
+	b_created, err := helper.MergeVersion(i1, i2)
 
-	t.Logf("%v & %v", i1, i2)
-	t.Log("hallo")
+	if err != nil {
+		t.Error("Should be able to handle two normal integers")
+		t.Fail()
+	}
+	if b_created != b1 {
+		t.Error("big integers should have the same values")
+		t.Fail()
+	}
+}
+func TestVersionMergeTwoNegatives(t *testing.T) {
 
+	_, err := helper.MergeVersion(int32(math.Pow(2, 32)-1), int32(math.Pow(2, 32)-1))
+	if err == nil {
+		t.Error("Should have caused an error")
+		t.Fail()
+	}
+}
+
+func TestVersionMergeOneNegativeOnePositive(t *testing.T) {
+
+	_, err := helper.MergeVersion(int32(math.Pow(2, 32)-1), int32(math.Pow(2, 1)))
+	if err == nil {
+		t.Error("Should have caused an error")
+		t.Fail()
+	}
+}
+
+func TestVersionMergeOnePositiveOneNegative(t *testing.T) {
+
+	_, err := helper.MergeVersion(int32(math.Pow(2, 1)), int32(math.Pow(2, 32)-1))
+	if err == nil {
+		t.Error("Should have caused an error")
+		t.Fail()
+	}
+}
+
+func TestVersionMergeTwoPositive(t *testing.T) {
+
+	big, err := helper.MergeVersion(int32(math.Pow(2, 31)-1), int32(math.Pow(2, 31)-1))
+	if err != nil {
+		t.Error("Should have been handled without error")
+		t.Fail()
+	}
+	if big != 0x3FFF_FFFF_FFFF_FFFF {
+		t.Errorf("Should have been equal to %v but was %v", 0x3FFF_FFFF_FFFF_FFFF, big)
+		t.Fail()
+	}
+}
+
+func TestVersionMergeTwoPositiveEx2(t *testing.T) {
+
+	big, err := helper.MergeVersion(int32(math.Pow(2, 30)-1), int32(math.Pow(2, 31)-1))
+	if err != nil {
+		t.Error("Should have been handled without error")
+		t.Fail()
+	}
+	if big != 0x1FFF_FFFF_FFFF_FFFF {
+		t.Errorf("Should have been equal to %v but was %v", 0x1FFF_FFFF_FFFF_FFFF, big)
+		t.Fail()
+	}
+}
+
+func TestVersionMergeTwoPositiveEx3(t *testing.T) {
+
+	big, err := helper.MergeVersion(int32(math.Pow(2, 30)-1), int32(math.Pow(2, 30)-1))
+	if err != nil {
+		t.Error("Should have been handled without error")
+		t.Fail()
+	}
+	if big != 0x1FFF_FFFF_BFFF_FFFF {
+		t.Errorf("Should have been equal to %v but was %v", 0x1FFF_FFFF_BFFF_FFFF, big)
+		t.Fail()
+	}
 }
