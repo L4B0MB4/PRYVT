2d9100bde1b60ee2a9b85fc867e69d49e07a7e5a
L4B0MB4 <metalfan65@gmail.com>
2024-09-17 18:34:13 +0200

    adding client server integration tests

---
+++ eventsourcing/pkg/integration_test/integration_test.go
@@ -0,0 +1,80 @@
+package integrationtest
+
+import (
+	"os"
+	"testing"
+
+	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/client"
+	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/httphandler"
+	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/httphandler/controller"
+	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/models"
+	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/store"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func setup() (*client.EventSourcingHttpClient, *httphandler.HttpHandler, *store.DatabaseConnection) {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	db := store.DatabaseConnection{}
+	db.SetUp()
+	conn, err := db.GetDbConnection()
+	if err != nil {
+		log.Error().Err(err).Msg("Unsuccessfull initalization of db")
+		panic(err)
+	}
+	log.Debug().Msg("Db Connection was successful")
+	repository := store.NewEventRepository(conn)
+
+	c := controller.NewEventController(repository)
+	h := httphandler.NewHttpHandler(c)
+
+	go func() {
+		h.Start()
+	}()
+	evclient, err := client.NewEventSourcingHttpClient("http://localhost:5515")
+	if err != nil {
+		panic(err)
+	}
+	return evclient, h, &db
+}
+
+func teardown(httpHandler *httphandler.HttpHandler, db *store.DatabaseConnection) {
+	httpHandler.Stop()
+	db.Teardown()
+}
+
+func TestClientAddingEventsAndRetrievingThemFromServer(t *testing.T) {
+	client, httpHandler, db := setup()
+	defer teardown(httpHandler, db)
+
+	err := client.AddEvents("myaggregate4444", []models.Event{{Version: 1, Name: "asdasd", Data: []byte{0, 1, 2}}, {Version: 2, Name: "asdasd2", Data: []byte{1, 2, 3}}})
+	if err != nil {
+		log.Error().Err(err).Msg("ERROR ADDING EVENTS")
+		t.Fail()
+	}
+	err = client.AddEvents("differentaggregate", []models.Event{{Version: 7, Name: "asdasd", Data: []byte{0, 1, 2}}, {Version: 8, Name: "asdasd2", Data: []byte{1, 2, 3}}})
+	if err != nil {
+		log.Error().Err(err).Msg("ERROR ADDING EVENTS FOR SECOND AGGREGATE")
+		t.Fail()
+	}
+	evs, err := client.GetEventsOrdered("myaggregate4444")
+	if err != nil {
+		log.Error().Err(err).Msg("ERROR RETRIEVING EVENTS")
+		t.Fail()
+	}
+	ev, ok := evs.Next()
+	if !ok {
+		log.Error().Err(err).Msg("DOES NOT HAVE ONE EVENT")
+	}
+	if ev.Version != 1 {
+		log.Error().Err(err).Msg("DOES HAVE WRONG FIRST EVENT")
+	}
+	ev, ok = evs.Next()
+	if !ok {
+		log.Error().Err(err).Msg("DOES NOT HAVE TWO EVENTS")
+	}
+	if ev.Version != 2 {
+		log.Error().Err(err).Msg("DOES HAVE WRONG SECOND EVENT")
+	}
+
+}
