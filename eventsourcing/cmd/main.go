9b48d6ab373d46b2584e01df421c0fc439a92bf9
L4B0MB4 <metalfan65@gmail.com>
2024-09-17 18:41:38 +0200

    only make server program available as build

--- eventsourcing/cmd/main.go
+++ eventsourcing/cmd/main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/client"
 	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/httphandler"
 	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/httphandler/controller"
-	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/models"
 	"gihtub.com/L4B0MB4/PRYVT/eventsouring/pkg/store"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
@@ -28,16 +25,5 @@ func main() {
 	c := controller.NewEventController(repository)
 	h := httphandler.NewHttpHandler(c)
 
-	go func() {
-		h.Start()
-	}()
-	evclient, err := client.NewEventSourcingHttpClient("http://localhost:5515")
-	if err != nil {
-		panic(err)
-	}
-	evclient.AddEvents("myaggregate4444", []models.Event{{Version: 2, Name: "asdasd2", Data: []byte{1, 2, 3}}})
-	evs, _ := evclient.GetEventsOrdered("myaggregate4444")
-	fmt.Println(evs)
-
-	h.Stop()
+	h.Start()
 }
